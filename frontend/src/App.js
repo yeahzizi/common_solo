0af24102fcfda48771a10ac9574349af29eb574a
yeahzizi <newyeahzizi@gmail.com>
2023-01-27 09:21:29 +0900

    navbar 틀 만들기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Room from './pages/Room/Room';
+import Recipe from './pages/Recipe/RecipeDetail';
+import Rank from './pages/Rank/TemperatureRank';
+import Login from './pages/User/Login/Login';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div>
-      <p className="test">테스트</p>
-      <p>테스트</p>
+      {/* <p className="test">테스트</p>
+      <p>테스트</p> */}
       {/* <p style={{ fontStyle: 'bold' }}>테스트</p> */}
+      <Switch>
+        <Route exact path="/main" component={Main} />
+        <Route path="/room" component={Room} />
+        <Route path="/recipe" component={Recipe} />
+        <Route path="/rank" component={Rank} />
+        <Route path="/login" component={Login} />
+      </Switch>
+      <Nav />
+      <hr />
     </div>
   );
 }
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import mainlogo from './mainlogo.png';
 
 function Nav() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <div>
+        <NavLink to="/main">
+          <img src={mainlogo} alt="mainlogo" width="100px" />
+        </NavLink>
+        <NavLink to="/room">요리방</NavLink>
+        <NavLink to="/recipe">레시피</NavLink>
+        <NavLink to="/rank">랭킹</NavLink>
+        <NavLink to="/login">로그인</NavLink>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
